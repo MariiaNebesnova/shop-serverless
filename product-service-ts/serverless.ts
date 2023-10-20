16504f48eeace42a5d743fbb838bf8df3e4d67e4
MariiaNebesnova <82033805+MariiaNebesnova@users.noreply.github.com>
2023-10-20 13:19:25 +0300

    add dotenv

--- product-service-ts/serverless.ts
+++ product-service-ts/serverless.ts
@@ -19,6 +19,8 @@ const serverlessConfiguration: AWS = {
     environment: {
       AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
       NODE_OPTIONS: '--enable-source-maps --stack-trace-limit=1000',
+      TABLE_PRODUCTS_NAME: "${env:TABLE_PRODUCTS_NAME}",
+      TABLE_STOCKS_NAME: "${env:TABLE_STOCKS_NAME}"
     },
     iam: {
       role: {
