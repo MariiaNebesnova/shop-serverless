b77a160aa33f4d9a14c5fe624962c502ba69851c
MariiaNebesnova <82033805+MariiaNebesnova@users.noreply.github.com>
2023-11-25 18:40:54 +0300

    add authorization-service

--- import-service/src/functions/importProductsFile/index.ts
+++ import-service/src/functions/importProductsFile/index.ts
@@ -8,6 +8,10 @@ export default {
       http: {
         method: 'get',
         path: 'import',
+        authorizer: {
+          arn: "arn:aws:lambda:us-east-1:784294038424:function:authorization-service-dev-basicAuthorizer",
+          type: "request",
+        },
         request: {
           parameters: {
             querystrings: {
